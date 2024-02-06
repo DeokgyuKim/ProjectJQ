b44925084c7a47904c67ae391ebdd4bfa7c522f7
theqqqq <kduck119@gmail.com>
2024-02-06 15:07:57 +0900

    Start Commit

---
+++ Source/ProjectJQ/ProjectJQGameMode.cpp
@@ -0,0 +1,26 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectJQGameMode.h"
+#include "ProjectJQPlayerController.h"
+#include "ProjectJQCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AProjectJQGameMode::AProjectJQGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = AProjectJQPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	// set default controller to our Blueprinted controller
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
+	if(PlayerControllerBPClass.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
